3e8c2fd4a6a9ae9701dc6d7088de223132a739fa
jill-gosrani <jillgosrani15@gmail.com>
2024-03-14 01:00:35 -0400

    added feature to add new blogs

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
-import { createRequire } from 'module';
-import { title } from "process";
 import fs from 'fs/promises';
 
-let json = fs.readFile('./allBlogs.json');
+let json = JSON.parse(await fs.readFile('./allBlogs.json', 'utf8'));
 console.log(json);
 const app = express();
 const port = 3000;
@@ -16,14 +14,20 @@ app.get("/",(req, res)=>{
     res.render("index.ejs");
 });
 
-app.post("/post",(req,res)=>{
-    res.render("newBlog.ejs",{
-        title: req.body["title"],
-        blogText : req.body["blog"],
-    });
-    json.title.push(title);
-    json.blogs.push(blogText);
-    json.ids.push(json.blogs.length -1);
+app.post("/add",(req,res)=>{
+    console.log("in add");
+    console.log(req.body);
+    let newBlogId = Object.keys(json).length;
+    json[newBlogId] = { title:req.body['title'], blog: req.body['blog'] };
+    let updatedJson = JSON.stringify(json, null, 2);
+    console.log(updatedJson);
+    fs.writeFile('./allBlogs.json', updatedJson);
+    res.redirect('/');
+});
+app.post("/new",(req,res)=>{
+    console.log("test");
+    res.render("./newBlog.ejs");
+    
 });
 
 app.listen(port,()=>{
