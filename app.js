49796e0d85c9e7ef1acc0634596d43319f2bf468
jill-gosrani <jillgosrani15@gmail.com>
2024-03-20 06:55:19 -0400

    New coding structure

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'dotenv/config';
+import expressEjsLayouts from 'express-ejs-layouts';
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+
+app.use(express.static('public'));
+
+app.use(expressEjsLayouts);
+app.set('layout', './layout/main');
+app.set('view engine', 'ejs');
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello!");
+});
+app.listen(PORT,()=>{
+    console.log(`Listening to port ${PORT}`);
+});
+
+
+
+
